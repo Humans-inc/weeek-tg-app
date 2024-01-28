e17a64bb641926da8e902137c1881e940b9a2aa2
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-01-28 22:41:45 +0100

    feat: add no-access page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
+import { NoAccess } from '../NoAccess/NoAccess';
 import styles from './App.module.scss';
 
 function App() {
   return (
-    <>
-      <h1 className={styles.mainTitle}>Weeek TG App</h1>
-    </>
+    <div className={styles.app}>
+      <NoAccess/>
+    </div>
   );
 }
 
---
+++ src/components/NoAccess/NoAccess.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+
+import styles from './NoAccess.module.scss';
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare global {
+  interface Window {
+    Telegram: any;
+  }
+}
+
+const NoAccess: FC = () => {
+  const tg = window.Telegram.WebApp;
+  console.log(tg);
+  tg.expand();
+  return (
+    <div className={styles.noAccess}>
+      <div className={`container ${styles.container}`}>
+        <img src="/images/humans.svg" alt="logo" width="150" />
+        <div className={styles.bottom}>
+          <h1 className={styles.bottomTitle}>У вас нет доступа</h1>
+          <p className={styles.bottomDescr}>
+            Попросите администратора бота<br/>выдать вам доступ к приложению
+          </p>
+          <button onClick={tg.close()} className={styles.bottomButton}>
+            Вернуться назад
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { NoAccess };
