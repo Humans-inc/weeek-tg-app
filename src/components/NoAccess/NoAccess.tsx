610252c36529e2d064726a9880719c642ff027b0
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-01-30 15:12:39 +0100

    fix: add listener for close app

--- src/components/NoAccess/NoAccess.tsx
+++ src/components/NoAccess/NoAccess.tsx
@@ -2,13 +2,6 @@ import { FC } from 'react';
 
 import styles from './NoAccess.module.scss';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare global {
-  interface Window {
-    Telegram: any;
-  }
-}
-
 const NoAccess: FC = () => {
   const tg = window.Telegram.WebApp;
   console.log(tg);
@@ -16,15 +9,15 @@ const NoAccess: FC = () => {
   return (
     <div className={styles.noAccess}>
       <div className={`container ${styles.container}`}>
-        <img src="/images/humans.svg" alt="logo" width="150" />
+        <img src="/webapp/images/humans.svg" alt="logo" width="150" />
         <div className={styles.bottom}>
           <h1 className={styles.bottomTitle}>У вас нет доступа</h1>
           <p className={styles.bottomDescr}>
-            Попросите администратора бота<br/>выдать вам доступ к приложению
+            Попросите администратора бота
+            <br />
+            выдать вам доступ к приложению
           </p>
-          <button className={styles.bottomButton}>
-            Вернуться назад
-          </button>
+          <button className={styles.bottomButton} onClick={() => tg.close()}>Вернуться назад</button>
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+declare global {
+  interface Window {
+    Telegram?: any;
+  }
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App/App';
