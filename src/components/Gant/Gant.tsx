4c325a670a0b99dc78e0cfcbd5c98bea42f2bb5f
Hanna Babai <kot.vmeshke.4@gmail.com>
2024-03-28 21:19:53 +0100

    refactor: gantt

--- src/components/Gant/Gant.tsx
+++ src/components/Gant/Gant.tsx
@@ -1,56 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import styles from './Gant.module.scss';
 import { GantProps } from '../../utils/types';
-import { Chart } from 'react-google-charts';
-
-const columns = [
-  { type: 'string', label: 'Task ID' },
-  { type: 'string', label: 'Task Name' },
-  { type: 'string', label: 'Resource' },
-  { type: 'date', label: 'Start Date' },
-  { type: 'date', label: 'End Date' },
-  { type: 'number', label: 'Duration' },
-  { type: 'number', label: 'Percent Complete' },
-  { type: 'string', label: 'Dependencies' },
-];
-
-const options = {
-  height: 700,
-  gantt: {
-    trackHeight: 30,
-    palette: [
-      {
-        color: '#7784ee',
-        dark: '#6d7be4',
-        light: '#97a1f0',
-      },
-    ],
-  },
-};
+import Gantt from 'frappe-gantt';
 
 const Gant: FC<GantProps> = ({ handleVisible, tasks }) => {
-  const filteredTask = [...tasks]
-    .map((task: any, index: number) => {
-      return [
-        task.id,
-        task.title || 'Noname',
-        task.boardId,
-        task.dateStart
-          ? new Date(task.dateStart.split('.').reverse().join('/'))
+  const frappeTasks = [...tasks]
+    .map((task) => {
+      return {
+        id: task.id,
+        name: task.title || 'Noname',
+        start: task.dateStart
+          ? task.dateStart.split('.').reverse().join('-')
           : '',
-        task.dateEnd
-          ? new Date(task.dateEnd.split('.').reverse().join('/'))
-          : '',
-        null,
-        0,
-        null,
-      ];
+        end: task.dateEnd ? task.dateEnd.split('.').reverse().join('-') : '',
+        progress: 100,
+        dependencies: '',
+        custom_class: '',
+      };
     })
-    .filter((item) => item[3] != '');
+    .filter((item) => item.start != '')
+    .sort((a, b) => +new Date(a.start) - +new Date(b.start));
 
-  const data = [columns, ...filteredTask];
+  const target = useRef<HTMLDivElement>(null);
+  const svg = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (svg.current) {
+      const gantt = new Gantt(svg.current, frappeTasks, {
+        language: 'ru',
+        view_mode: 'Day',
+      });
+      console.log(gantt);
+    }
+    const midOfSvg = svg.current!.clientWidth * 0.5;
+    target.current!.scrollLeft = midOfSvg;
+  }, [frappeTasks]);
 
   return (
     <div className={styles.gant}>
@@ -72,14 +58,15 @@ const Gant: FC<GantProps> = ({ handleVisible, tasks }) => {
       </button>
       <div className={`container ${styles.gantContainer}`}>
         <div className={styles.gantWrapper}>
-          <Chart
-            chartType="Gantt"
-            width="100%"
-            height="100%"
-            data={data}
-            options={options}
-            chartLanguage='ru'
-          />
+          <div style={{ overflow: 'scroll' }} ref={target}>
+            <svg
+              ref={svg}
+              width="100%"
+              height="100%"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            />
+          </div>
         </div>
       </div>
     </div>
